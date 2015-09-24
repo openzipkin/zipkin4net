eff9e24483079768289d0788d825975c6468da2b
Unknown <unknown@criteo.com>
2015-09-24 09:52:39 +0200

    Add Trace context passing over HTTP
    Change-Id: Ia8177f3aa41f16a4f4b7a36ef50891264ef55efd
    JIRA: CSI-946

---
+++ Criteo.Profiling.Tracing.UTest/Transport/T_HttpTraceContext.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Specialized;
+using Criteo.Profiling.Tracing.Transport;
+using NUnit.Framework;
+
+namespace Criteo.Profiling.Tracing.UTest.Transport
+{
+    [TestFixture]
+    class T_HttpTraceContext
+    {
+
+        [Test]
+        public void SerializedTraceIsEqualToOriginal()
+        {
+            var originalTrace = Trace.Create();
+
+            var headers = new NameValueCollection();
+
+            HttpTraceContext.Set(headers, originalTrace);
+
+            Trace receivedTrace;
+            Assert.True(HttpTraceContext.TryGet(headers, out receivedTrace));
+
+            Assert.AreEqual(originalTrace, receivedTrace);
+        }
+
+        [Test]
+        public void DeserializedTraceIsEqualToOriginal()
+        {
+            var headers = new NameValueCollection();
+            headers[HttpTraceContext.TraceId] = "0000000000000001";
+            headers[HttpTraceContext.ParentSpanId] = "0000000000000000";
+            headers[HttpTraceContext.SpanId] = "00000000000000FA";
+            headers[HttpTraceContext.Flags] = "0";
+
+            Trace receivedTrace;
+            Assert.True(HttpTraceContext.TryGet(headers, out receivedTrace));
+
+            var recreatedHeaders = new NameValueCollection();
+            HttpTraceContext.Set(recreatedHeaders, receivedTrace);
+
+            Assert.AreEqual(headers.Count, recreatedHeaders.Count);
+
+            Assert.AreEqual(headers[HttpTraceContext.TraceId], recreatedHeaders[HttpTraceContext.TraceId]);
+            Assert.AreEqual(headers[HttpTraceContext.ParentSpanId], recreatedHeaders[HttpTraceContext.ParentSpanId]);
+            Assert.AreEqual(headers[HttpTraceContext.SpanId], recreatedHeaders[HttpTraceContext.SpanId]);
+            Assert.AreEqual(headers[HttpTraceContext.Flags], recreatedHeaders[HttpTraceContext.Flags]);
+        }
+
+        [Test]
+        public void HeadersAreCorrectlySet()
+        {
+
+            var traceId = new SpanId(1, 0, 250, Flags.Empty().SetSampled());
+            var trace = Trace.CreateFromId(traceId);
+
+            var headers = new NameValueCollection();
+
+            HttpTraceContext.Set(headers, trace);
+
+            Assert.AreEqual(5, headers.Count);
+
+            Assert.AreEqual("0000000000000001", headers[HttpTraceContext.TraceId]);
+            Assert.AreEqual("0000000000000000", headers[HttpTraceContext.ParentSpanId]);
+            Assert.AreEqual("00000000000000FA", headers[HttpTraceContext.SpanId]);
+            Assert.AreEqual("6", headers[HttpTraceContext.Flags]);
+            Assert.AreEqual("1", headers[HttpTraceContext.Sampled]);
+        }
+
+        [Test]
+        public void GetWithoutFlagsIsCorrectlyCreated()
+        {
+            var headers = new NameValueCollection();
+            headers[HttpTraceContext.TraceId] = "0000000000000001";
+            headers[HttpTraceContext.ParentSpanId] = "0000000000000000";
+            headers[HttpTraceContext.SpanId] = "00000000000000FA";
+
+            Trace trace;
+            Assert.True(HttpTraceContext.TryGet(headers, out trace));
+
+            Assert.AreEqual(1, trace.CurrentId.TraceId);
+            Assert.AreEqual(0, trace.CurrentId.ParentSpanId);
+            Assert.AreEqual(250, trace.CurrentId.Id);
+            Assert.AreEqual(Flags.Empty(), trace.CurrentId.Flags);
+        }
+
+        [Test]
+        [Description("A missing required trace header should prevent the trace to be created (required headers are traceId, spanId and parentSpanId)")]
+        public void GetWithoutRequiredHeadersShouldFail()
+        {
+            var headers = new NameValueCollection();
+            headers[HttpTraceContext.Flags] = "0";
+            headers[HttpTraceContext.Sampled] = "1";
+
+            Trace trace;
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+
+            headers[HttpTraceContext.TraceId] = "0000000000000001";
+            headers[HttpTraceContext.SpanId] = null;
+            headers[HttpTraceContext.ParentSpanId] = null;
+
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+
+            headers[HttpTraceContext.TraceId] = null;
+            headers[HttpTraceContext.SpanId] = "00000000000000FA";
+            headers[HttpTraceContext.ParentSpanId] = null;
+
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+
+            headers[HttpTraceContext.TraceId] = null;
+            headers[HttpTraceContext.SpanId] = null;
+            headers[HttpTraceContext.ParentSpanId] = "0000000000000000";
+
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+
+            headers[HttpTraceContext.TraceId] = null;
+            headers[HttpTraceContext.SpanId] = "00000000000000FA";
+            headers[HttpTraceContext.ParentSpanId] = "0000000000000000";
+
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+
+            headers[HttpTraceContext.TraceId] = "0000000000000001";
+            headers[HttpTraceContext.SpanId] = "00000000000000FA";
+            headers[HttpTraceContext.ParentSpanId] = null;
+
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+
+            headers[HttpTraceContext.TraceId] = "0000000000000001";
+            headers[HttpTraceContext.SpanId] = null;
+            headers[HttpTraceContext.ParentSpanId] = "0000000000000000";
+
+            Assert.False(HttpTraceContext.TryGet(headers, out trace));
+        }
+
+        [Test]
+        public void LongIdEncodingIsCorrect()
+        {
+
+            const long notEncodedLong = 170;
+            const string expectedEncodedLong = "00000000000000AA";
+
+            var encodedLong = HttpTraceContext.EncodeLongToHexString(notEncodedLong);
+
+            Assert.AreEqual(expectedEncodedLong, encodedLong);
+        }
+
+        [Test]
+        public void LongIdDecodingIsCorrect()
+        {
+            const string encodedLong = "00000000000000AA";
+            const long expectedLong = 170;
+
+            var decodedLong = HttpTraceContext.DecodeHexString(encodedLong);
+            Assert.AreEqual(expectedLong, decodedLong);
+        }
+
+        [Test]
+        [Description("Check that encode is the inverse function of decode. In other words that x = encode(decode(x)) and y = decode(encode(y))")]
+        public void IdEncodingDecodingGivesOriginalValues()
+        {
+            const long input = 10;
+
+            var encoded = HttpTraceContext.EncodeLongToHexString(input);
+            var decoded = HttpTraceContext.DecodeHexString(encoded);
+
+            Assert.AreEqual(input, decoded);
+
+            const string encodedInput = "00000000000000AA";
+            var decodedInput = HttpTraceContext.DecodeHexString(encodedInput);
+            var reEncodedInput = HttpTraceContext.EncodeLongToHexString(decodedInput);
+
+            Assert.AreEqual(encodedInput, reEncodedInput);
+        }
+
+    }
+}
---
+++ Criteo.Profiling.Tracing/Transport/HttpTraceContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace Criteo.Profiling.Tracing.Transport
+{
+    public class HttpTraceContext
+    {
+        public const string TraceId = "X-B3-TraceId";
+        public const string SpanId = "X-B3-SpanId";
+        public const string ParentSpanId = "X-B3-ParentSpanId";
+        public const string Sampled = "X-B3-Sampled"; // Will be replaced by Flags in the future releases of Finagle
+        public const string Flags = "X-B3-Flags";
+
+        public static bool TryGet(NameValueCollection headers, out Trace trace)
+        {
+            var encodedTraceId = headers[TraceId];
+            var encodedSpanId = headers[SpanId];
+            var encodedParentSpanId = headers[ParentSpanId];
+
+            if (!String.IsNullOrWhiteSpace(encodedTraceId) && !String.IsNullOrWhiteSpace(encodedSpanId) &&
+                !String.IsNullOrWhiteSpace(encodedParentSpanId))
+            {
+                try
+                {
+                    var traceId = DecodeHexString(encodedTraceId);
+                    var spanId = DecodeHexString(encodedSpanId);
+                    var parentSpanId = DecodeHexString(encodedParentSpanId);
+                    var sampled = ParseSampledHeader(headers[Sampled]);
+                    var flags = ParseFlagsHeader(headers[Flags]);
+
+                    if (sampled != null)
+                        flags = sampled.Value ? flags.SetSampled() : flags.SetNotSampled();
+
+                    var id = new SpanId(traceId, parentSpanId, spanId, flags);
+                    trace = Trace.CreateFromId(id);
+                    return true;
+                }
+                catch (Exception _)
+                {
+                }
+            }
+
+            trace = default(Trace);
+            return false;
+        }
+
+        public static void Set(NameValueCollection headers, Trace trace)
+        {
+            var traceId = trace.CurrentId;
+
+            headers[TraceId] = EncodeLongToHexString(traceId.TraceId);
+            headers[SpanId] = EncodeLongToHexString(traceId.Id);
+            headers[ParentSpanId] = EncodeLongToHexString(traceId.ParentSpanId);
+            headers[Flags] = traceId.Flags.ToLong().ToString(CultureInfo.InvariantCulture);
+
+            // Add "Sampled" header for compatibility with Finagle
+            if (traceId.Flags.IsSamplingKnown())
+                headers[Sampled] = traceId.Flags.IsSampled() ? "1" : "0";
+        }
+
+        internal static string EncodeLongToHexString(long value)
+        {
+            return value.ToString("X16");
+        }
+
+        internal static long DecodeHexString(String longAsHexString)
+        {
+            return Convert.ToInt64(longAsHexString, 16);
+        }
+
+        private static bool? ParseSampledHeader(String header)
+        {
+            if (String.Equals(header, "1"))
+            {
+                return true;
+            }
+
+            if (String.Equals(header, "0"))
+            {
+                return false;
+            }
+
+            return null;
+        }
+
+        private static Flags ParseFlagsHeader(String header)
+        {
+            long flagsLong;
+
+            if (!String.IsNullOrEmpty(header) && Int64.TryParse(header, out flagsLong))
+            {
+                return Tracing.Flags.FromLong(flagsLong);
+            }
+
+            return Tracing.Flags.Empty();
+        }
+
+
+    }
+}
