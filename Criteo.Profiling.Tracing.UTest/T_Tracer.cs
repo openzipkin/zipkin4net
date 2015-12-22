6ee0ecb72da09e9ed0f8a8fa965ca6f37fb40a93
Unknown <unknown@criteo.com>
2015-12-22 11:32:57 +0100

    Add tests on exception catching and multiple start()
    Change-Id: Ic144127447ab78b008cb98ef568a9c5b19dee79a
    JIRA: CSI-1183

--- Criteo.Profiling.Tracing.UTest/T_Tracer.cs
+++ Criteo.Profiling.Tracing.UTest/T_Tracer.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Moq;
 using NUnit.Framework;
 
@@ -7,38 +8,39 @@ namespace Criteo.Profiling.Tracing.UTest
     [TestFixture]
     class T_Tracer
     {
+        private Mock<ILogger> _mockLogger;
+        private Mock<ITracer> _mockTracer;
 
         [SetUp]
         public void Setup()
         {
             Tracer.Clear();
+
+            _mockLogger = new Mock<ILogger>();
+            Trace.Logger = _mockLogger.Object;
+
+            _mockTracer = new Mock<ITracer>();
+            Tracer.Register(_mockTracer.Object);
         }
 
         [Test]
         public void TracersAreCorrectlyRegistered()
         {
-            var tracer = new Mock<ITracer>();
-
-            Tracer.Register(tracer.Object);
-
             Assert.AreEqual(1, Tracer.Tracers.Count);
-            Assert.AreEqual(tracer.Object, Tracer.Tracers.First());
+            Assert.AreEqual(_mockTracer.Object, Tracer.Tracers.First());
 
-            var secondTracer = new Mock<ITracer>();
+            var secondMockTracer = new Mock<ITracer>();
 
-            Tracer.Register(secondTracer.Object);
+            Tracer.Register(secondMockTracer.Object);
 
             Assert.AreEqual(2, Tracer.Tracers.Count);
-            Assert.True(Tracer.Tracers.Contains(tracer.Object));
-            Assert.True(Tracer.Tracers.Contains(secondTracer.Object));
+            Assert.True(Tracer.Tracers.Contains(_mockTracer.Object));
+            Assert.True(Tracer.Tracers.Contains(secondMockTracer.Object));
         }
 
         [Test]
         public void TracersAreIndeedCleared()
         {
-            var tracer = new Mock<ITracer>();
-            Tracer.Register(tracer.Object);
-
             var secondTracer = new Mock<ITracer>();
             Tracer.Register(secondTracer.Object);
 
@@ -49,6 +51,19 @@ namespace Criteo.Profiling.Tracing.UTest
             Assert.AreEqual(0, Tracer.Tracers.Count);
         }
 
+        [Test]
+        public void ExceptionsAreCatchedWhileRecording()
+        {
+            const string errorMsg = "Something bad happened somewhere";
+
+            _mockTracer.Setup(tracer1 => tracer1.Record(It.IsAny<Record>())).Throws(new Exception(errorMsg));
+
+            var record = new Record(new SpanId(0, null, 1, Flags.Empty()), DateTime.UtcNow, Annotations.ClientRecv());
+
+            Assert.DoesNotThrow(() => Tracer.Push(record));
+
+            _mockLogger.Verify(logger1 => logger1.LogWarning(It.Is<string>(s => s.Contains(errorMsg))), Times.Once());
+        }
 
     }
 }
