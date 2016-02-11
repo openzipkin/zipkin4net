dac70656ede6a3c7e5f5b292b4897679a9243b6a
Unknown <unknown@criteo.com>
2016-02-11 17:23:56 +0100

    Override Annotations "Equals()"
    Change-Id: Iad5db6d7bcce9ba3f3617c619e915151f1bb42eb

--- Criteo.Profiling.Tracing/Annotation/LocalAddr.cs
+++ Criteo.Profiling.Tracing/Annotation/LocalAddr.cs
@@ -17,6 +17,19 @@ namespace Criteo.Profiling.Tracing.Annotation
             return String.Format("{0} {1}:{2}", GetType().Name, EndPoint.Address, EndPoint.Port);
         }
 
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return ((LocalAddr)obj).EndPoint.Equals(EndPoint);
+        }
+
+        public override int GetHashCode()
+        {
+            return (EndPoint != null ? EndPoint.GetHashCode() : 0);
+        }
+
         public void Accept(IAnnotationVisitor visitor)
         {
             visitor.Visit(this);
--- Criteo.Profiling.Tracing/Annotation/Rpc.cs
+++ Criteo.Profiling.Tracing/Annotation/Rpc.cs
@@ -16,6 +16,19 @@ namespace Criteo.Profiling.Tracing.Annotation
             return String.Format("{0}: {1}", GetType().Name, Name);
         }
 
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return string.Equals(Name, ((Rpc)obj).Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
+
         public void Accept(IAnnotationVisitor visitor)
         {
             visitor.Visit(this);
