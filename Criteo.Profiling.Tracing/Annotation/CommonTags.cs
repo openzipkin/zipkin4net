57f376d375ea2400fa38b76475cf1f37f9906be7
Unknown <unknown@criteo.com>
2016-02-26 13:25:36 +0100

    Add common tags
    Change-Id: I3d2136d6457d0da9afd3fee598ead4f4b2f17a1e

---
+++ Criteo.Profiling.Tracing/Annotation/CommonTags.cs
@@ -0,0 +1,10 @@
+﻿using Criteo.Profiling.Tracing.Tracers.Zipkin.Thrift;
+
+namespace Criteo.Profiling.Tracing.Annotation
+{
+    public static class CommonTags
+    {
+        public static readonly string HttpStatusCode = zipkinCoreConstants.HTTP_STATUS_CODE;
+        public static readonly string HttpMethod = zipkinCoreConstants.HTTP_METHOD;
+    }
+}
