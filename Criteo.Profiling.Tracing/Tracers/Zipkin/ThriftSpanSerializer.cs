27b2337820976590cc19daae5f4a51dac5689efd
Unknown <unknown@criteo.com>
2016-03-10 14:37:11 +0100

    Specify duration in Span
    It is now advised to set the duration in the span
    instead of letting the Zipkin Query service computate it from the timestamps.
    
    Change-Id: Ifee025a33d920fc5dde63562b88857500daa0911

--- Criteo.Profiling.Tracing/Tracers/Zipkin/ThriftSpanSerializer.cs
+++ Criteo.Profiling.Tracing/Tracers/Zipkin/ThriftSpanSerializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using Criteo.Profiling.Tracing.Tracers.Zipkin.Thrift;
 using Criteo.Profiling.Tracing.Utils;
@@ -55,43 +56,72 @@ namespace Criteo.Profiling.Tracing.Tracers.Zipkin
                 Service_name = spanServiceName
             };
 
-            foreach (var ann in span.Annotations)
+            var thriftAnnotations = span.Annotations.Select(ann => ConvertToThrift(ann, host)).ToList();
+            if (thriftAnnotations.Count > 0)
             {
-                var annThrift = ConvertToThrift(ann);
-
-                annThrift.Host = host;
-
-                if (thriftSpan.Annotations == null)
-                    thriftSpan.Annotations = new List<Thrift.Annotation>();
-
-                thriftSpan.Annotations.Add(annThrift);
+                thriftSpan.Annotations = thriftAnnotations;
             }
 
-            foreach (var binaryAnnotation in span.BinaryAnnotations)
+            var thriftBinaryAnnotations = span.BinaryAnnotations.Select(ann => ConvertToThrift(ann, host)).ToList();
+            if (thriftBinaryAnnotations.Count > 0)
             {
-                var annBinThrift = ConvertToThrift(binaryAnnotation);
+                thriftSpan.Binary_annotations = thriftBinaryAnnotations;
+            }
 
-                annBinThrift.Host = host;
-
-                if (thriftSpan.Binary_annotations == null)
-                    thriftSpan.Binary_annotations = new List<Thrift.BinaryAnnotation>();
-
-                thriftSpan.Binary_annotations.Add(annBinThrift);
+            // Duration should now be specified
+            var duration = span.Complete ? ComputeSpanDuration(span.Annotations) : null;
+            if (duration.HasValue && duration.Value.TotalMilliseconds > 0)
+            {
+                thriftSpan.Duration = (long)(duration.Value.TotalMilliseconds * 1000); // microseconds
             }
 
             return thriftSpan;
+        }
+
+        private static TimeSpan? ComputeSpanDuration(ICollection<ZipkinAnnotation> annotations)
+        {
+            DateTime? startTime = null;
+            DateTime? endTime = null;
+
+            // Priority is for the client duration
+            var clientSendAnn = annotations.FirstOrDefault(a => a.Value.Equals(zipkinCoreConstants.CLIENT_SEND));
+            if (clientSendAnn != default(ZipkinAnnotation))
+            {
+                startTime = clientSendAnn.Timestamp;
+
+                var clientRcvAnn = annotations.FirstOrDefault(a => a.Value.Equals(zipkinCoreConstants.CLIENT_RECV));
+                if (clientRcvAnn != null) endTime = clientRcvAnn.Timestamp;
+            }
+            else
+            {
+                // Else look for server annotations
+                foreach (var ann in annotations)
+                {
+                    if (ann.Value.Equals(zipkinCoreConstants.SERVER_RECV))
+                    {
+                        startTime = ann.Timestamp;
+                    }
+                    else if (ann.Value.Equals(zipkinCoreConstants.SERVER_SEND))
+                    {
+                        endTime = ann.Timestamp;
+                    }
+                }
+            }
+
+            return (startTime.HasValue && endTime.HasValue) ? (TimeSpan?)endTime.Value.Subtract(startTime.Value) : null;
         }
 
         /// <summary>
         /// Convert this annotation object to its Thrift equivalent.
         /// </summary>
         /// <returns></returns>
-        public static Thrift.Annotation ConvertToThrift(ZipkinAnnotation zipkinAnnotation)
+        public static Thrift.Annotation ConvertToThrift(ZipkinAnnotation zipkinAnnotation, Thrift.Endpoint host)
         {
             var thriftAnn = new Thrift.Annotation
             {
                 Timestamp = TimeUtils.ToUnixTimestamp(zipkinAnnotation.Timestamp),
-                Value = zipkinAnnotation.Value
+                Value = zipkinAnnotation.Value,
+                Host = host
             };
 
             return thriftAnn;
@@ -101,13 +131,14 @@ namespace Criteo.Profiling.Tracing.Tracers.Zipkin
         /// Convert this span binary annotation object to its Thrift equivalent.
         /// </summary>
         /// <returns></returns>
-        public static Thrift.BinaryAnnotation ConvertToThrift(BinaryAnnotation binaryAnnotation)
+        public static Thrift.BinaryAnnotation ConvertToThrift(BinaryAnnotation binaryAnnotation, Thrift.Endpoint host)
         {
             return new Thrift.BinaryAnnotation
             {
                 Annotation_type = binaryAnnotation.AnnotationType,
                 Key = binaryAnnotation.Key,
-                Value = binaryAnnotation.Value
+                Value = binaryAnnotation.Value,
+                Host = host
             };
         }
 
