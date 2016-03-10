9c9c257d1c50c76f9df9ad7461427c3f31ed44e4
Unknown <unknown@criteo.com>
2016-03-10 16:50:37 +0100

    Add tests on Statistics
    Change-Id: Id9a24d518971dd6ee5553748f0f1985504b29285

---
+++ Criteo.Profiling.Tracing.UTest/Tracers/Zipkin/T_Statistics.cs
@@ -0,0 +1,40 @@
+﻿using Criteo.Profiling.Tracing.Tracers.Zipkin;
+using NUnit.Framework;
+
+namespace Criteo.Profiling.Tracing.UTest.Tracers.Zipkin
+{
+    [TestFixture]
+    class T_Statistics
+    {
+
+        [Test]
+        public void InitialValueAreZeros()
+        {
+            var stats = new Statistics();
+
+            Assert.AreEqual(0, stats.RecordProcessed);
+            Assert.AreEqual(0, stats.SpanFlushed);
+            Assert.AreEqual(0, stats.SpanSent);
+            Assert.AreEqual(0, stats.SpanSentTotalBytes);
+        }
+
+        [Test]
+        public void ValuesAreUpdated()
+        {
+            var stats = new Statistics();
+
+            stats.UpdateRecordProcessed();
+            Assert.AreEqual(1, stats.RecordProcessed);
+
+            stats.UpdateSpanFlushed();
+            Assert.AreEqual(1, stats.SpanFlushed);
+
+            stats.UpdateSpanSent();
+            Assert.AreEqual(1, stats.SpanSent);
+
+            stats.UpdateSpanSentBytes(10);
+            Assert.AreEqual(10, stats.SpanSentTotalBytes);
+        }
+
+    }
+}
