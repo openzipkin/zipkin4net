07cdd2d55deacc32c00c08db8341dee6f4cd02d1
Unknown <unknown@criteo.com>
2016-05-11 18:40:26 +0200

    Introduce TraceContext using LogicalCallContext
    Change-Id: Ieb693af6b519b1568fbe955a1cde49dd72cb280e

---
+++ Criteo.Profiling.Tracing/TraceContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Remoting.Messaging;
+
+namespace Criteo.Profiling.Tracing
+{
+    internal static class TraceContext
+    {
+        private const string TraceCallContextKey = "crto_trace";
+
+        public static Trace Get()
+        {
+            return CallContext.LogicalGetData(TraceCallContextKey) as Trace;
+        }
+
+        public static void Set(Trace trace)
+        {
+            CallContext.LogicalSetData(TraceCallContextKey, trace);
+        }
+
+        public static void Clear()
+        {
+            CallContext.FreeNamedDataSlot(TraceCallContextKey);
+        }
+    }
+}
