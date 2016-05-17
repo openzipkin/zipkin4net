d701465058f3a55ebec3b5f60365a17a5b9fe9b9
Unknown <unknown@criteo.com>
2016-05-17 15:54:15 +0000

    Revert "Revert "Introduce TraceContext using LogicalCallContext""
    This reverts commit f3a8d40595bc208162944f8a9580f31f7d414bd4.
    
    Change-Id: I795acad9544605af63db52cfdb6586a375566c6f
    JIRA: CSI-1390

---
+++ Criteo.Profiling.Tracing.UTest/T_TraceContext.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace Criteo.Profiling.Tracing.UTest
+{
+    [TestFixture]
+    class T_TraceContext
+    {
+        [Test]
+        public void ContextCanBeSetThenGetAndCleared()
+        {
+            var trace = Trace.Create();
+            TraceContext.Set(trace);
+            Assert.AreEqual(trace, TraceContext.Get());
+
+            TraceContext.Clear();
+            Assert.IsNull(TraceContext.Get());
+        }
+
+        [Test]
+        public void SetOverridesPreviousTrace()
+        {
+            var firstTrace = Trace.Create();
+            var secondTrace = Trace.Create();
+
+            TraceContext.Set(firstTrace);
+            TraceContext.Set(secondTrace);
+
+            Assert.AreEqual(secondTrace, TraceContext.Get());
+        }
+
+        [Test]
+        public void ContextIsPassedToCreatedThreads()
+        {
+            var trace = Trace.Create();
+            TraceContext.Set(trace);
+
+            Trace threadTrace = null;
+            Trace threadTrace2 = null;
+
+            var thread = new Thread(() =>
+            {
+                threadTrace = TraceContext.Get();
+            });
+            thread.Start();
+
+            TraceContext.Clear();
+
+            var thread2 = new Thread(() =>
+            {
+                threadTrace2 = TraceContext.Get();
+            });
+            thread2.Start();
+
+            thread.Join();
+            thread2.Join();
+
+            Assert.AreEqual(trace, threadTrace);
+            Assert.IsNull(threadTrace2);
+        }
+    }
+}
