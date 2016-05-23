535ad09a57cc22f473d7f75df292dd8a96082304
Unknown <unknown@criteo.com>
2016-05-23 18:24:23 +0200

    Add check for Mono for LogicalCallContext
    Some projects are running an outdated Mono version which does not implement LogicalCallContext.
    
    Change-Id: I07d90a8cd3c909c17bbd9e856816ce625379512f

--- Criteo.Profiling.Tracing/TraceContext.cs
+++ Criteo.Profiling.Tracing/TraceContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Remoting.Messaging;
+﻿using System;
+using System.Runtime.Remoting.Messaging;
 
 namespace Criteo.Profiling.Tracing
 {
@@ -6,18 +7,26 @@ namespace Criteo.Profiling.Tracing
     {
         private const string TraceCallContextKey = "crto_trace";
 
+        private static readonly bool IsRunningOnMono = (Type.GetType("Mono.Runtime") != null);
+
         public static Trace Get()
         {
+            if (IsRunningOnMono) return null;
+
             return CallContext.LogicalGetData(TraceCallContextKey) as Trace;
         }
 
         public static void Set(Trace trace)
         {
+            if (IsRunningOnMono) return;
+
             CallContext.LogicalSetData(TraceCallContextKey, trace);
         }
 
         public static void Clear()
         {
+            if (IsRunningOnMono) return;
+
             CallContext.FreeNamedDataSlot(TraceCallContextKey);
         }
     }
