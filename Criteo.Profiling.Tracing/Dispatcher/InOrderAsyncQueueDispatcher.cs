dbed5f9e77919a813b2ae174af880980ce4f5894
Unknown <unknown@criteo.com>
2016-09-22 19:14:44 +0200

    QueueDispatcher: wait for all messages to be processed
    Change-Id: Ia886b466165631eea9c570179aba8879beec6fa4

--- Criteo.Profiling.Tracing/Dispatcher/InOrderAsyncQueueDispatcher.cs
+++ Criteo.Profiling.Tracing/Dispatcher/InOrderAsyncQueueDispatcher.cs
@@ -10,17 +10,21 @@ namespace Criteo.Profiling.Tracing.Dispatcher
     /// </summary>
     internal class InOrderAsyncQueueDispatcher : IRecordDispatcher
     {
+        private const int WaitSleepTimeMs = 500;
+
+        private readonly int _timeoutOnStopMs;
         private readonly BlockingCollection<Record> _queue;
         private readonly CancellationTokenSource _cancellationTokenSource;
 
-        public InOrderAsyncQueueDispatcher(Action<Record> pushToTracers, int maxCapacity = 5000)
+        public InOrderAsyncQueueDispatcher(Action<Record> pushToTracers, int maxCapacity = 5000, int timeoutOnStopMs = 10000)
         {
+            _timeoutOnStopMs = timeoutOnStopMs;
             _queue = new BlockingCollection<Record>(maxCapacity);
             _cancellationTokenSource = new CancellationTokenSource();
 
             Task.Factory.StartNew(() =>
             {
-                while (!_cancellationTokenSource.IsCancellationRequested)
+                while (!_queue.IsCompleted && !_cancellationTokenSource.IsCancellationRequested)
                 {
                     var record = _queue.Take(_cancellationTokenSource.Token);
                     pushToTracers(record);
@@ -30,12 +34,26 @@ namespace Criteo.Profiling.Tracing.Dispatcher
 
         public void Stop()
         {
+            _queue.CompleteAdding();
+
+            int maxWaitLoop = _timeoutOnStopMs / WaitSleepTimeMs;
+
+            while (maxWaitLoop > 0 && !_queue.IsCompleted)
+            {
+                Thread.Sleep(WaitSleepTimeMs);
+                maxWaitLoop--;
+            }
+
             _cancellationTokenSource.Cancel(throwOnFirstException: false);
         }
 
         public bool Dispatch(Record record)
         {
-            return _queue.TryAdd(record);
+            if (!_queue.IsAddingCompleted)
+            {
+                return _queue.TryAdd(record);
+            }
+            return false;
         }
     }
 }
