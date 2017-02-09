115ac64c1e14de494303d8464c841d066563cb3a
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-02-09 15:28:02 +0000

    Open source compliance
    Change-Id: Ife428c4466f034799f4143dc1fd373642fc58e08

--- Criteo.Profiling.Tracing/Properties/AssemblyInfo.cs
+++ Criteo.Profiling.Tracing/Properties/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Reflection;
+﻿// Copyright 2017 Criteo
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +21,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Criteo.Profiling.Tracing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Criteo")]
 [assembly: AssemblyProduct("Criteo.Profiling.Tracing")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © Criteo 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +45,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Criteo.Diagnostic.UTest")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 [assembly: InternalsVisibleTo("Criteo.Profiling.Tracing.UTest")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
