88c265d99ee9847060e29ff9a55be7e636e819da
Pierre-Hugues Jeanneret <fedj@free.fr>
2017-02-21 07:33:18 -0400

    Add build script

---
+++ buildAndTest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+function check_availability() {
+    binary=$1
+    which $binary 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "$binary could not be found in PATH"
+        exit 1
+    fi
+}
+
+src="Criteo.Profiling.Tracing"
+tests="Criteo.Profiling.Tracing.UTest"
+
+check_availability "dotnet"
+
+dotnet restore
+dotnet build $src
+dotnet build $tests
+dotnet test $tests
