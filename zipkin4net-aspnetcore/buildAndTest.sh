663802a751781033a3d975f2ae4ac8687ebba6c5
Pierre-Hugues Jeanneret <fedj@free.fr>
2017-03-13 10:43:57 +0100

    ASP.Net core zipkin Middleware
    Provides a middleware to trace incoming requests to an ASP.Net server.

---
+++ zipkin4net-aspnetcore/buildAndTest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function check_availability() {
+    binary=$1
+    which $binary 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "$binary could not be found in PATH"
+        exit 1
+    fi
+}
+
+src="Criteo.Profiling.Tracing.Middleware"
+
+check_availability "dotnet"
+
+dotnet restore         \
+&& dotnet build $src   \
