cc03bf90bdba56caa56611308f6e6cdf146b6ec2
Pierre-Hugues Jeanneret <fedj@free.fr>
2017-03-13 15:19:11 +0100

    Trace Http client calls.
    The tracing handler now also sends CS and CR to the collector.

--- zipkin4net-aspnetcore/Criteo.Profiling.Tracing.Middleware/TracingHandler.cs
+++ zipkin4net-aspnetcore/Criteo.Profiling.Tracing.Middleware/TracingHandler.cs
@@ -8,19 +8,29 @@ namespace Criteo.Profiling.Tracing.Middleware
     public class TracingHandler : DelegatingHandler
     {
         private readonly ITraceInjector<HttpHeaders> _injector;
+        private readonly string _serviceName;
 
-        public TracingHandler(ITraceInjector<HttpHeaders> injector)
+        public TracingHandler(ITraceInjector<HttpHeaders> injector, string serviceName)
         {
             _injector = injector;
+            _serviceName = serviceName;
         }
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
         {
             var trace = Trace.Current;
             if (trace != null)
             {
-                _injector.Inject(Trace.Current, request.Headers);
+                trace = trace.Child();
+                _injector.Inject(trace, request.Headers);
             }
-            return base.SendAsync(request, cancellationToken);
+            trace.Record(Annotations.ClientSend());
+            trace.Record(Annotations.ServiceName(_serviceName));
+            trace.Record(Annotations.Rpc(request.Method.ToString()));
+            return base.SendAsync(request, cancellationToken)
+                .ContinueWith(t => {
+                    trace.Record(Annotations.ClientRecv());
+                    return t.Result;
+                });
         }
     }
 }
