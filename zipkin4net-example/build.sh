bceb4d8a80cd50747b38569bd85f3742fc564bb7
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-03-14 15:37:43 +0100

    Add build script for example

---
+++ zipkin4net-example/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash                                                                                                                                                           
+
+function check_availability() {
+    binary=$1
+    which $binary 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "$binary could not be found in PATH"
+	exit 1
+    fi
+}
+
+check_availability "dotnet"
+
+dotnet restore         \
+&& dotnet build "common"   \
+&& dotnet build "backend"   \
+&& dotnet build "frontend"   \
+
