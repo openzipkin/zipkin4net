97a1c8c12d6552da01c23b4712f0f3800fd9cda3
Pierre-Hugues Jeanneret <fedj@free.fr>
2017-03-14 14:05:30 +0100

    Provide a simple use of zipkin4net.
    A frontend service calls a backend service.
    To call them, just do dotnet run http://*:5000 on frontend for example.

---
+++ zipkin4net-example/common/CommonStartup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Criteo.Profiling.Tracing;
+using Criteo.Profiling.Tracing.Tracers.Zipkin;
+using Criteo.Profiling.Tracing.Transport;
+using Criteo.Profiling.Tracing.Transport.Http;
+using Criteo.Profiling.Tracing.Middleware;
+using System.Net.Http;
+using System.Threading;
+
+namespace common
+{
+    abstract public class CommonStartup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            
+            var config = ConfigureSettings.CreateConfiguration();
+            
+            var applicationName = config["applicationName"];
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            var lifetime = app.ApplicationServices.GetService<IApplicationLifetime>();
+            lifetime.ApplicationStarted.Register(() => {
+                TraceManager.SamplingRate = 1.0f;
+                var logger = new TracingLogger(loggerFactory, "Criteo.Profiling.Tracing");
+                var httpSender = new HttpZipkinSender( "http://localhost:9411");
+                var tracer = new ZipkinTracer(httpSender);
+                TraceManager.RegisterTracer(tracer);
+                TraceManager.Start(logger);
+            });
+            lifetime.ApplicationStopped.Register(() => TraceManager.Stop());
+            app.UseTracing(applicationName);
+            Run(app, config);
+        }
+
+        abstract protected void Run(IApplicationBuilder app, IConfiguration configuration);
+    }
+}
