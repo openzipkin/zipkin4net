efd28e2a06c35a16e7ecf3fd845da1f72e606263
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-03-16 11:55:38 +0100

    Make the build fail if a sub-component fails to build
    A bug was introduced when we switched to sub-projects, leading to
    successful build status even if a compilation failed.

--- buildAndTest.sh
+++ buildAndTest.sh
@@ -6,6 +6,10 @@ for i in "${repositories[@]}"; do
     if [ -d $i ]; then
 	pushd $i
 	./buildAndTest.sh
+        if [ $? -ne 0 ]; then
+          echo "Compilation of $i failed, exiting"
+          exit 1
+        fi
 	popd
     else
 	echo "Can't build $i"
