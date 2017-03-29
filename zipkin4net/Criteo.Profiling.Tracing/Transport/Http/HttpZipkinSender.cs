1d387749fd182cfe1a2ad033cda862911ba600cf
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-03-29 11:58:02 +0200

    Inject content-type to HttpZipkinSender

--- zipkin4net/Criteo.Profiling.Tracing/Transport/Http/HttpZipkinSender.cs
+++ zipkin4net/Criteo.Profiling.Tracing/Transport/Http/HttpZipkinSender.cs
@@ -1,4 +1,5 @@
 using Criteo.Profiling.Tracing.Tracers.Zipkin;
+using System;
 using System.Net.Http;
 
 namespace Criteo.Profiling.Tracing.Transport.Http
@@ -7,21 +8,28 @@ namespace Criteo.Profiling.Tracing.Transport.Http
     {
         private string _zipkinCollectorUrl;
         private HttpClient _httpClient;
+        private readonly string _contentType;
 
+        [Obsolete("Please specify the content type. E.g. application/x-thrift for thrift and application/json for JSON)")]
         public HttpZipkinSender(string zipkinCollectorUrl)
-        : this(new HttpClient(), zipkinCollectorUrl)
+        : this(zipkinCollectorUrl, "application/x-thrift")
         {}
 
-        internal HttpZipkinSender(HttpClient httplient, string zipkinCollectorUrl)
+        public HttpZipkinSender(string zipkinCollectorUrl, string contentType)
+        : this(new HttpClient(), zipkinCollectorUrl, contentType)
+        {}
+
+        internal HttpZipkinSender(HttpClient httplient, string zipkinCollectorUrl, string contentType)
         {
             _zipkinCollectorUrl = ForgeSpansEndpoint(zipkinCollectorUrl);
             _httpClient = httplient;
+            _contentType = contentType;
         }
 
         public void Send(byte[] data)
         {
             var content = new ByteArrayContent(data);
-            content.Headers.Add("Content-Type", "application/x-thrift");
+            content.Headers.Add("Content-Type", _contentType);
             content.Headers.Add("Content-Length", data.Length.ToString());
             _httpClient.PostAsync(_zipkinCollectorUrl, content);
         }
