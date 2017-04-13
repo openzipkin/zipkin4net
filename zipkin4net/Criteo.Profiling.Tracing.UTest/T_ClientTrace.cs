6aa8e83c2a6740e5272300b5d7aca368123cac23
Pierre-Hugues Jeanneret <fedj@free.fr>
2017-04-13 16:55:55 +0200

    Fix NPE if CurrentTrace is null while using ClientTrace

---
+++ zipkin4net/Criteo.Profiling.Tracing.UTest/T_ClientTrace.cs
@@ -0,0 +1,34 @@
+using Criteo.Profiling.Tracing;
+using NUnit.Framework;
+
+namespace Criteo.Profiling.Tracing.UTest
+{
+    [TestFixture]
+    internal class T_ClientTrace
+    {
+        private const string serviceName = "service1";
+        private const string rpc = "rpc";
+
+        [Test]
+        public void ShouldNotSetCurrentTrace()
+        {
+            Trace.Current = null;
+            using (var client = new ClientTrace(serviceName, rpc))
+            {
+                Assert.IsNull(client.Trace);
+            }
+        }
+
+        [Test]
+        public void ShouldCallChildWhenCurrentTraceNotNull()
+        {
+            var trace = Trace.Create();
+            Trace.Current = trace;
+            using (var client = new ClientTrace(serviceName, rpc))
+            {
+                Assert.AreEqual(trace.CurrentSpan.SpanId, client.Trace.CurrentSpan.ParentSpanId);
+                Assert.AreEqual(trace.CurrentSpan.TraceId, client.Trace.CurrentSpan.TraceId);
+            }
+        }
+    }
+}
