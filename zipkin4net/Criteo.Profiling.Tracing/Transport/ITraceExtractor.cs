fe05d8330633f851dd330ec574a47093f2b02a96
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-04-20 14:52:07 +0200

    Provide a way to mutualize propagation.
    Before, we had to almost copy/paste the entire injection/extraction methods.
    An extractor and injector function have been introduced.

--- zipkin4net/Criteo.Profiling.Tracing/Transport/ITraceExtractor.cs
+++ zipkin4net/Criteo.Profiling.Tracing/Transport/ITraceExtractor.cs
@@ -1,5 +1,26 @@
-﻿namespace Criteo.Profiling.Tracing.Transport
+﻿using System;
+
+namespace Criteo.Profiling.Tracing.Transport
 {
+    /**
+    * Distributed tracing extraction interface
+    *
+    * Extract a trace from a carrier. It can for example be used
+    * to extract B3 headers from HTTP headers to recreate a trace
+    * object.
+    */
+    public interface ITraceExtractor
+    {
+        bool TryExtract<TE>(TE carrier, Func<TE, string, string> extractor, out Trace trace);
+    }
+
+    /**
+    * Distributed tracing extraction interface
+    *
+    * Extract a trace from a carrier. It can for example be used
+    * to extract B3 headers from HTTP headers to recreate a trace
+    * object.
+    */
     public interface ITraceExtractor<in TE>
     {
         bool TryExtract(TE carrier, out Trace trace);
--- zipkin4net/Criteo.Profiling.Tracing/Transport/ITraceInjector.cs
+++ zipkin4net/Criteo.Profiling.Tracing/Transport/ITraceInjector.cs
@@ -1,5 +1,26 @@
-﻿namespace Criteo.Profiling.Tracing.Transport
+﻿using System;
+
+namespace Criteo.Profiling.Tracing.Transport
 {
+    /**
+    * Distributed tracing injection interface.
+    *
+    * Inject into a carrier informations needed to propagate
+    * a trace. It can for example be used to propagate
+    * B3 headers in http headers.
+    */
+    public interface ITraceInjector
+    {
+        bool Inject<TE>(Trace trace, TE carrier, Action<TE, string, string> injector);
+    }
+
+    /**
+    * Distributed tracing injection interface.
+    *
+    * Inject into a carrier informations needed to propagate
+    * a trace. It can for example be used to propagate
+    * B3 headers in http headers.
+    */
     public interface ITraceInjector<in TE>
     {
         bool Inject(Trace trace, TE carrier);
