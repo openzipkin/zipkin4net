fc10c669db9e8e5b64ec9213bfdc6c21b93f3c50
Corentin Pescheloche <c.pescheloche@criteo.com>
2017-07-25 12:55:51 +0200

    Deprecate TraceHelper and create extension method on ClientTrace and ServerTrace. The goal of this commit is to centralize trace building into ClientTrace and ServerTrace. Later on those two classes will define virtual methods to allow users to customize the behavior when receiving and sending a request and when an error is thrown (eg add exception's stacktrace instead of message)

--- zipkin4net/Criteo.Profiling.Tracing/ServerTrace.cs
+++ zipkin4net/Criteo.Profiling.Tracing/ServerTrace.cs
@@ -1,5 +1,7 @@
 using Criteo.Profiling.Tracing.Annotation;
 using System;
+using System.Threading.Tasks;
+
 namespace Criteo.Profiling.Tracing
 {
     public class ServerTrace : IDisposable
@@ -23,10 +25,36 @@ namespace Criteo.Profiling.Tracing
         {
             Trace.Record(annotation);
         }
-
+        
         public void Dispose()
         {
             Trace.Record(Annotations.ServerSend());
+        }
+
+        public virtual void Error(Exception ex)
+        {
+            Trace.RecordAnnotation(Annotations.Tag("error", ex.Message));
+        }
+    }
+
+    public static class ServerTraceExtensions
+    {
+        /// <summary>
+        /// Runs the task asynchronously and adds an error annotation in case of failure
+        /// </summary>
+        /// <param name="serverTrace"></param>
+        /// <param name="task"></param>
+        public static async Task TracedActionAsync(this ServerTrace serverTrace, Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                serverTrace?.Error(ex);
+                throw;
+            }
         }
     }
 }
