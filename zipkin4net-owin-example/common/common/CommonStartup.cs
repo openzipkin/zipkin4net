b06f1f8511e23ea79377c9286293124f8563ebd8
klettier <lettier.kevin@gmail.com>
2017-08-02 22:02:44 +0200

    Add Owin middleware + Example

---
+++ zipkin4net-owin-example/common/common/CommonStartup.cs
@@ -0,0 +1,57 @@
+﻿using Criteo.Profiling.Tracing;
+using Criteo.Profiling.Tracing.Tracers.Zipkin;
+using Criteo.Profiling.Tracing.Transport.Http;
+using Microsoft.Owin.BuilderProperties;
+using Owin;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace common
+{
+    public abstract class CommonStartup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: GetRouteTemplate(),
+                defaults: null,
+                constraints: null,
+                handler: GetHandler()
+            );
+
+            //Setup tracing
+            TraceManager.SamplingRate = 1.0f;
+            var logger = new ConsoleLogger();
+            var httpSender = new HttpZipkinSender("http://localhost:9411", "application/json");
+            var tracer = new ZipkinTracer(httpSender, new JSONSpanSerializer());
+            TraceManager.RegisterTracer(tracer);
+            TraceManager.Start(logger);
+            //
+
+            //Stop TraceManager on app dispose
+            var properties = new AppProperties(appBuilder.Properties);
+            var token = properties.OnAppDisposing;
+
+            if (token != CancellationToken.None)
+            {
+                token.Register(() =>
+                {
+                    TraceManager.Stop();
+                });
+            }
+            //
+
+            // Owin Middleware
+            appBuilder.UseZipkinTracer(System.Configuration.ConfigurationManager.AppSettings["applicationName"]);
+
+            appBuilder.UseWebApi(config);
+        }
+
+        protected abstract HttpMessageHandler GetHandler();
+        protected virtual string GetRouteTemplate() => "";
+    }
+}
---
+++ zipkin4net-owin-example/frontend/frontend/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using common;
+using System.Threading.Tasks;
+using System.Threading;
+using Criteo.Profiling.Tracing.Middleware;
+
+namespace frontend
+{
+    public class Startup : CommonStartup
+    {
+        protected override HttpMessageHandler GetHandler() => new CallApiHandler();
+
+        class CallApiHandler : HttpMessageHandler
+        {
+            private static readonly string callServiceUrl = System.Configuration.ConfigurationManager.AppSettings["callServiceUrl"];
+            private static readonly string applicationName = System.Configuration.ConfigurationManager.AppSettings["applicationName"];
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                using (var httpClient = new HttpClient(new TracingHandler(applicationName)))
+                {
+                    var response = await httpClient.GetAsync(callServiceUrl);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return response;
+                    }
+
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    return request.CreateResponse(System.Net.HttpStatusCode.OK, content);
+                }
+            }
+        }
+    }
+}
