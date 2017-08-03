6eae99ad078fc621c56c2b93314a4b27b107e534
klettier <lettier.kevin@gmail.com>
2017-08-03 07:39:57 +0200

    Move one step up

--- zipkin4net-owin-example/run.cmd
+++ zipkin4net-owin-example/run.cmd
@@ -2,5 +2,5 @@
 
 msbuild zipkin4net-owin-example.sln
 
-start backend\backend\bin\Debug\backend.exe "http://localhost:9000"
-start frontend\frontend\bin\Debug\frontend.exe "http://localhost:8081"
+start backend\bin\Debug\backend.exe "http://localhost:9000"
+start frontend\bin\Debug\frontend.exe "http://localhost:8081"
