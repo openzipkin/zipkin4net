7096f64d4c274b965489c41ca8f77e0c5a74c5e0
klettier <lettier.kevin@gmail.com>
2017-08-05 23:22:38 +0200

    Add tests

---
+++ zipkin4net-owin/Criteo.Profiling.Tracing.Middleware.Tests/Helpers/OwinHelper.cs
@@ -0,0 +1,37 @@
+﻿using Criteo.Profiling.Tracing.Transport;
+using Microsoft.Owin.Testing;
+using Owin;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Criteo.Profiling.Tracing.Middleware.Tests.Helpers
+{
+    static class OwinHelper
+    {
+        internal static async Task Call(Action<IAppBuilder> startup, Func<HttpClient, Task> clientCall)
+        {
+            using (var server = TestServer.Create(startup))
+            {
+                using (var client = new HttpClient(server.Handler))
+                {
+                    await clientCall(client);
+                }
+            }
+        }
+        internal static Action<IAppBuilder> DefaultStartup(string serviceName, ITraceExtractor traceExtractor)
+        {
+            return
+                app =>
+                {
+                    app.UseZipkinTracer(serviceName, traceExtractor);
+
+                    app.Run(context =>
+                    {
+                        context.Response.ContentType = "text/plain";
+                        return context.Response.WriteAsync(DateTime.Now.ToString());
+                    });
+                };
+        }
+    }
+}
--- zipkin4net-owin/Criteo.Profiling.Tracing.Middleware.Tests/Properties/AssemblyInfo.cs
+++ zipkin4net-owin/Criteo.Profiling.Tracing.Middleware.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Criteo.Profiling.Tracing.Middleware")]
+[assembly: AssemblyTitle("Criteo.Profiling.Tracing.Middleware.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Criteo.Profiling.Tracing.Middleware")]
+[assembly: AssemblyProduct("Criteo.Profiling.Tracing.Middleware.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6a57c709-4443-4735-94cf-19de60b00f59")]
+[assembly: Guid("3ddf4522-e86b-40cd-87e4-0ccd132860b5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Criteo.Profiling.Tracing.Middleware.Tests")]
