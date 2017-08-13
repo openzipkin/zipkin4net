347bd8b1a37a354ae0e30c9074229c2e333bb1e4
klettier <lettier.kevin@gmail.com>
2017-08-13 16:33:44 +0200

    Run  tests

--- zipkin4net-owin/buildAndTest.sh
+++ zipkin4net-owin/buildAndTest.sh
@@ -2,3 +2,4 @@
 
 nuget restore ./zipkin4net-owin.sln
 xbuild /p:Configuration=Release ./zipkin4net-owin.sln
+mono packages/NUnit.Runners.2.6.4/tools/nunit-console.exe Criteo.Profiling.Tracing.Middleware.Tests/bin/Release/Criteo.Profiling.Tracing.Middleware.Tests.dll
