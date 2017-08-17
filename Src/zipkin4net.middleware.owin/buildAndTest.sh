2c1fd15e58343e65b3e4f51fb38ec6045c37200a
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-08-17 15:37:06 +0200

    Revamp the repository layout.

---
+++ Src/zipkin4net.middleware.owin/buildAndTest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+nuget restore ./zipkin4net.middleware.owin.sln \
+&& xbuild /p:Configuration=Release ./zipkin4net.middleware.owin.sln \
+&& mono packages/NUnit.Runners.2.6.4/tools/nunit-console.exe Tests/bin/Release/Criteo.Profiling.Tracing.Middleware.Tests.dll
