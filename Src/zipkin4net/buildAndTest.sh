1bfcac4cead306402de0fd6510a8621c264452c5
Guillaume <guillaume1998@free.fr>
2017-08-28 11:35:32 +0200

    Switch to .Net Core 2.0 for Tests and Benchmark

--- Src/zipkin4net/buildAndTest.sh
+++ Src/zipkin4net/buildAndTest.sh
@@ -18,6 +18,6 @@ check_availability "dotnet"
 
 dotnet restore  $solution   \
 && dotnet build -f "netstandard1.5" $src         \
-&& dotnet build -f "netcoreapp1.1" $benchmark         \
-&& dotnet build -f "netcoreapp1.0" $tests        \
-&& dotnet test -f "netcoreapp1.0" $tests           \
+&& dotnet build -f "netcoreapp2.0" $benchmark         \
+&& dotnet build -f "netcoreapp2.0" $tests        \
+&& dotnet test -f "netcoreapp2.0" $tests           \
