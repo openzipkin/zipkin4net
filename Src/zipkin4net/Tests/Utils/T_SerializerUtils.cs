5f119a4914447fd54a12b4c52d5cccd608635f1a
anthonywanted <anthonywanted@163.com>
2017-11-03 17:31:39 +0800

    JSONSpanSerializer add ToEscaped while serializing serviceName, span.Name, annotation.Value, binaryAnnotation.Value (#157)
    * JSONSpanSerializer Add ToEscaped while serializing serviceName, span.Name, annotation.Value, binaryAnnotation.Value
    
    * Add test
    
    * JSONSpanSerializer escape annotation.Value too.
    Fix a stupid mistake

---
+++ Src/zipkin4net/Tests/Utils/T_SerializerUtils.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using zipkin4net.Tracers.Zipkin;
+
+namespace zipkin4net.Tests.dotnetcore.Utils
+{
+    [TestFixture]
+    internal class T_SerializerUtils
+    {
+        [Test]
+        public void ToEscaped()
+        {
+            string testString = "\"testString\"";
+            string testString2 = "\"!@#$%^&*()\a\b\0\f\n\r\t\v";
+            Assert.AreEqual(SerializerUtils.ToEscaped(testString), "\\\"testString\\\"");
+            Assert.AreEqual(SerializerUtils.ToEscaped(testString2), "\\\"!@#$%^&*()\\a\\b\\0\\f\\n\\r\\t\\v");
+
+        }
+    }
+}
