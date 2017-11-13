70ed4b43a3731209323a3baf066869fdc26a3899
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-11-13 09:10:21 +0000

    Move compilation from fake to dotnet cli

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-nuget restore ./packages.config -PackagesDirectory ./build-packages && mono build-packages/FAKE.4.63.0/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+dotnet restore zipkin4net.dotnetcore.sln
+msbuild zipkin4net.dotnetcore.sln
+dotnet test -f netcoreapp2.0 Src/zipkin4net/Tests/zipkin4net.Tests.dotnetcore.csproj
