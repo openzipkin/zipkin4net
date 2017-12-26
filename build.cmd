a351c76c9c99ad8483ecb52e78a3cbb91da00f08
klettier <lettier.kevin@gmail.com>
2017-12-26 18:28:43 +0100

    Prepare Owin middleware release

--- build.cmd
+++ build.cmd
@@ -2,6 +2,6 @@
 cls
 
 dotnet restore "zipkin4net.dotnetcore.sln"
-dotnet build "zipkin4net.dotnetcore.sln"
-dotnet test "Src/zipkin4net/Tests/zipkin4net.Tests.dotnetcore.csproj"
-dotnet test "Src/zipkin4net.middleware.owin/Tests/zipkin4net.middleware.owin.Tests.dotnetcore.csproj"
+dotnet build "zipkin4net.dotnetcore.sln" --no-restore
+dotnet test "Src/zipkin4net/Tests/zipkin4net.Tests.dotnetcore.csproj" --no-restore --no-build
+dotnet test "Src/zipkin4net.middleware.owin/Tests/zipkin4net.middleware.owin.Tests.dotnetcore.csproj" --no-restore --no-build
