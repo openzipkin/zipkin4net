129c8385dec535c83633dd08c7be597558d997db
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-12-28 10:05:04 +0100

    Remove project files previous to VS2017
    It allows to ease maintenance when creating files (not having to maintain multiple csprojs anymore)

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-dotnet restore zipkin4net.dotnetcore.sln
-msbuild zipkin4net.dotnetcore.sln
-dotnet test -f netcoreapp2.0 Src/zipkin4net/Tests/zipkin4net.Tests.dotnetcore.csproj
+dotnet restore zipkin4net.sln
+msbuild zipkin4net.sln
+dotnet test -f netcoreapp2.0 Src/zipkin4net/Tests/zipkin4net.Tests.csproj
