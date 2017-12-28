0b04f94d5f2f99a9c45297719eeb82bb64c449a8
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2017-12-28 10:05:04 +0100

    Remove project files previous to VS2017
    It allows to ease maintenance when creating files (not having to maintain multiple csprojs anymore)

--- Examples/owin/run.cmd
+++ Examples/owin/run.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-msbuild zipkin4net-owin-example.sln
+msbuild owin-example.sln
 
 start backend\bin\Debug\backend.exe "http://localhost:9000"
 start frontend\bin\Debug\frontend.exe "http://localhost:8081"
--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 cls
 
-dotnet restore "zipkin4net.dotnetcore.sln"
-dotnet build "zipkin4net.dotnetcore.sln" --no-restore
-dotnet test "Src/zipkin4net/Tests/zipkin4net.Tests.dotnetcore.csproj" --no-restore --no-build
-dotnet test "Src/zipkin4net.middleware.owin/Tests/zipkin4net.middleware.owin.Tests.dotnetcore.csproj" --no-restore --no-build
+dotnet restore "zipkin4net.sln"
+dotnet build "zipkin4net.sln" --no-restore
+dotnet test "Src/zipkin4net/Tests/zipkin4net.Tests.csproj" --no-restore --no-build
+dotnet test "Src/zipkin4net.middleware.owin/Tests/zipkin4net.middleware.owin.Tests.csproj" --no-restore --no-build
