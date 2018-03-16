91fd4b0431104b73da2be02984ee25741f35f33c
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2018-03-16 09:13:58 +0100

    Add span v2 datamodel.
    Also, datamodel is made for Endpoint and Annotation

---
+++ Src/zipkin4net/Src/Internal/V2/Annotation.cs
@@ -0,0 +1,23 @@
+﻿namespace zipkin4net.Internal.V2
+{
+    internal struct Annotation
+    {
+        /// <summary>
+        /// Microseconds from epoch.
+        /// 
+        /// <p>This value should be set directly by instrumentation, using the most precise value possible.
+        /// </summary>
+        public readonly long Timestamp;
+
+        /// <summary>
+        /// Usually a short tag indicating an event, like <code>cache.miss</code> or <code>error</code> 
+        /// </summary>
+        public readonly string Value;
+
+        public Annotation(long timestamp, string value)
+        {
+            Timestamp = timestamp;
+            Value = value;
+        }
+    }
+}
