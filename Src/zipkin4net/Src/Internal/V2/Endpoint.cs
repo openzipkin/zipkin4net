71914bc8a7475c64773e73d06e240341479a8be6
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2018-03-16 09:13:58 +0100

    Add span v2 datamodel.
    Also, datamodel is made for Endpoint and Annotation

---
+++ Src/zipkin4net/Src/Internal/V2/Endpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace zipkin4net.Internal.V2
+{
+    internal struct Endpoint
+    {
+        public readonly string ServiceName;
+        public readonly string Ipv4;
+
+        public Endpoint(string serviceName, string ipv4)
+        {
+            ServiceName = serviceName;
+            Ipv4 = ipv4;
+        }
+    }
+}
