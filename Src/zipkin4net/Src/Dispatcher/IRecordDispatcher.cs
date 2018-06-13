7c5b3de8c7c9b22f299945997d655cb569cb0a43
Christian Palmstierna <christian.palmstierna@gmail.com>
2018-06-13 13:57:13 +0200

    Make IRecordDispatcher public and exchangeable

--- Src/zipkin4net/Src/Dispatcher/IRecordDispatcher.cs
+++ Src/zipkin4net/Src/Dispatcher/IRecordDispatcher.cs
@@ -1,10 +1,15 @@
 ﻿namespace zipkin4net.Dispatcher
 {
-    internal interface IRecordDispatcher
+    public interface IRecordDispatcher
     {
-
+        /// <summary>
+        /// Stops the dispatcher.
+        /// </summary>
         void Stop();
-
+        /// <summary>
+        /// Dispatches a record to the registered tracers.
+        /// </summary>
+        /// <returns>True if the record was dispatched. Otherwise false.</returns>
         bool Dispatch(Record record);
 
     }
