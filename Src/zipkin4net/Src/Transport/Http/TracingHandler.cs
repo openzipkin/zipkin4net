a64301096e9d92e5a7171a41eba2f53be59109a9
Benjamin Hugot <bhugot@gmail.com>
2018-07-26 15:34:00 +0200

    Some comments

--- Src/zipkin4net/Src/Transport/Http/TracingHandler.cs
+++ Src/zipkin4net/Src/Transport/Http/TracingHandler.cs
@@ -12,11 +12,22 @@ namespace zipkin4net.Transport.Http
         private readonly string _serviceName;
         private readonly Func<HttpRequestMessage, string> _getClientTraceRpc;
 
+        /// <summary>
+        /// Create a Tracing Handler
+        /// </summary>
+        /// <param name="serviceName"></param>
+        /// <param name="httpMessageHandler">if not set or null then it set an <see cref="HttpClientHandler"/> as inner handler</param>
+        /// <param name="getClientTraceRpc"></param>
         public TracingHandler(string serviceName, HttpMessageHandler httpMessageHandler = null, Func<HttpRequestMessage, string> getClientTraceRpc = null)
         : this(Propagations.B3String.Injector<HttpHeaders>((carrier, key, value) => carrier.Add(key, value)), serviceName, httpMessageHandler ?? new HttpClientHandler(), getClientTraceRpc)
         { }
 
-        
+        /// <summary>
+        /// Create a TracingHandler for injection purpose like HttpClientFactory in AspNetCore
+        /// </summary>
+        /// <param name="serviceName"></param>
+        /// <param name="getClientTraceRpc"></param>
+        /// <returns></returns>
         public static TracingHandler WithoutInnerHandler(string serviceName, Func<HttpRequestMessage, string> getClientTraceRpc = null)
          =>  new TracingHandler(Propagations.B3String.Injector<HttpHeaders>((carrier, key, value) => carrier.Add(key, value)), serviceName, getClientTraceRpc);
 
@@ -27,6 +38,13 @@ namespace zipkin4net.Transport.Http
             _serviceName = serviceName;
             _getClientTraceRpc = getClientTraceRpc ?? (request => request.Method.ToString());
         }
+
+        /// <summary>
+        /// Constructor used to create the handler without an inner handler.
+        /// </summary>
+        /// <param name="injector"></param>
+        /// <param name="serviceName"></param>
+        /// <param name="getClientTraceRpc"></param>
         private TracingHandler(IInjector<HttpHeaders> injector, string serviceName, Func<HttpRequestMessage, string> getClientTraceRpc = null)
         {
             _injector = injector;
