6e7332d39b8182b76cbbde9ba76ba5c6a68393ec
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2018-09-04 16:14:00 +0200

    Add implementation of B3 propagation for B3 headers only

---
+++ Src/zipkin4net/Src/Propagation/B3SingleInjector.cs
@@ -0,0 +1,19 @@
+﻿namespace zipkin4net.Propagation
+{
+    internal class B3SingleInjector<C, K> : IInjector<C>
+    {
+        private readonly B3SinglePropagation<K> _b3Propagation;
+        private readonly Setter<C, K> _setter;
+
+        public B3SingleInjector(B3SinglePropagation<K> b3Propagation, Setter<C, K> setter)
+        {
+            _b3Propagation = b3Propagation;
+            _setter = setter;
+        }
+
+        public void Inject(ITraceContext traceContext, C carrier)
+        {
+            _setter(carrier, _b3Propagation.B3Key, B3SingleFormat.WriteB3SingleFormat(traceContext));
+        }
+    }
+}
