e6fdc44e1d871be05371192508adc07c214ce0d1
Pierre-Hugues Jeanneret <ph.jeanneret@criteo.com>
2018-09-04 16:14:00 +0200

    Add implementation of B3 propagation for B3 headers only

---
+++ Src/zipkin4net/Src/Propagation/B3SingleExtractor.cs
@@ -0,0 +1,23 @@
+﻿namespace zipkin4net.Propagation
+{
+    internal class B3SingleExtractor<C, K> : IExtractor<C>
+    {
+        private readonly B3SinglePropagation<K> _b3SinglePropagation;
+        private readonly Getter<C, K> _getter;
+
+        private const int TraceId64BitsSerializationLength = 16;
+
+        internal B3SingleExtractor(B3SinglePropagation<K> b3SinglePropagation, Getter<C, K> getter)
+        {
+            _b3SinglePropagation = b3SinglePropagation;
+            _getter = getter;
+        }
+
+        public ITraceContext Extract(C carrier)
+        {
+            return B3SingleFormat.ParseB3SingleFormat(
+                _getter(carrier, _b3SinglePropagation.B3Key)
+            );
+        }
+    }
+}
