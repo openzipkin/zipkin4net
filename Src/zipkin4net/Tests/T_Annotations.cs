7ce228e083eca3410523ae47ef08a78a33a9c7a8
Danila Polevshchikov <danila.polevshikov@gmail.com>
2018-09-13 11:29:06 +0200

    Annotation's ToString provides all extra data in addition to annotation name

--- Src/zipkin4net/Tests/T_Annotations.cs
+++ Src/zipkin4net/Tests/T_Annotations.cs
@@ -1,4 +1,5 @@
-﻿using zipkin4net.Annotation;
+﻿using System.Net;
+using zipkin4net.Annotation;
 using NUnit.Framework;
 
 namespace zipkin4net.UTest
@@ -26,6 +27,23 @@ namespace zipkin4net.UTest
             Assert.IsInstanceOf<ConsumerStop>(Annotations.ConsumerStop());
             Assert.IsInstanceOf<ProducerStart>(Annotations.ProducerStart());
             Assert.IsInstanceOf<ProducerStop>(Annotations.ProducerStop());
+            Assert.IsInstanceOf<LocalOperationStart>(Annotations.LocalOperationStart(""));
+            Assert.IsInstanceOf<LocalOperationStop>(Annotations.LocalOperationStop());
+        }
+
+        [Test]
+        public void ToStringWriteExtraDataInAdditionToType()
+        {
+            Assert.AreEqual("TagAnnotation: sampleTagKey [sampleTagValue System.String]", Annotations.Tag("sampleTagKey", "sampleTagValue").ToString());
+            Assert.AreEqual("Rpc: GET", Annotations.Rpc("GET").ToString());
+            Assert.AreEqual("ServiceName: sampleName", Annotations.ServiceName("sampleName").ToString());
+            Assert.AreEqual("Event: sampleName", Annotations.Event("sampleName").ToString());
+            Assert.AreEqual("LocalOperationStart: sampleName", Annotations.LocalOperationStart("sampleName").ToString());
+
+            var samIpEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+            Assert.AreEqual("ClientAddr: 127.0.0.1:80", Annotations.ClientAddr(samIpEndPoint).ToString());
+            Assert.AreEqual("ServerAddr: sampleName/127.0.0.1:80", Annotations.ServerAddr("sampleName", samIpEndPoint).ToString());
+            Assert.AreEqual("MessageAddr: sampleName/127.0.0.1:80", Annotations.MessageAddr("sampleName", samIpEndPoint).ToString());
         }
 
     }
