f8f0d7b2335ee0e311824f365d3f83cdfac39777
Brian Stauff <brian.stauff.github@gmail.com>
2019-02-05 15:58:20 -0600

    Touch up OwinExtensions with the new constructor args

--- Src/zipkin4net.middleware.owin/Src/Extensions/OwinExtensions.cs
+++ Src/zipkin4net.middleware.owin/Src/Extensions/OwinExtensions.cs
@@ -7,10 +7,12 @@ namespace Owin
 {
     public static class OwinExtensions
     {
-        public static IAppBuilder UseZipkinTracer(this IAppBuilder appBuilder, string serviceName, IExtractor<IHeaderDictionary> traceExtractor, Func<IOwinContext, string> getRpc = null)
-            => appBuilder.Use<ZipkinMiddleware>(serviceName, traceExtractor, getRpc);
+        public static IAppBuilder UseZipkinTracer(this IAppBuilder appBuilder, string serviceName, IExtractor<IHeaderDictionary> traceExtractor, Func<IOwinContext, string> getRpc = null,
+            Func<PathString, bool> routeFilter = null)
+            => appBuilder.Use<ZipkinMiddleware>(serviceName, traceExtractor, getRpc, routeFilter);
 
-        public static IAppBuilder UseZipkinTracer(this IAppBuilder appBuilder, string serviceName, Func<IOwinContext, string> getRpc = null)
-            => appBuilder.UseZipkinTracer(serviceName, Propagations.B3String.Extractor((IHeaderDictionary carrier, string key) => string.Join(",", carrier[key])), getRpc);
+        public static IAppBuilder UseZipkinTracer(this IAppBuilder appBuilder, string serviceName, Func<IOwinContext, string> getRpc = null,
+            Func<PathString, bool> routeFilter = null)
+            => appBuilder.UseZipkinTracer(serviceName, Propagations.B3String.Extractor((IHeaderDictionary carrier, string key) => string.Join(",", carrier[key])), getRpc, routeFilter);
     }
 }
