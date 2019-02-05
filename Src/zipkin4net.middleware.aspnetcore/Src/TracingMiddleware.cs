0e823f0212c761d3e5cac88dcdc8262bda77136b
Brian Stauff <brian.stauff.github@gmail.com>
2019-02-05 11:50:09 -0600

    Added route filtering for Owin and AspNetCore middleware

--- Src/zipkin4net.middleware.aspnetcore/Src/TracingMiddleware.cs
+++ Src/zipkin4net.middleware.aspnetcore/Src/TracingMiddleware.cs
@@ -9,9 +9,12 @@ namespace zipkin4net.Middleware
     public static class TracingMiddleware
     {
         public static void UseTracing(this IApplicationBuilder app, string serviceName,
-            Func<HttpContext, string> getRpc = null)
+            Func<HttpContext, string> getRpc = null,
+            Func<PathString, bool> routeFilter = null)
         {
             getRpc = getRpc ?? (context => context.Request.Method);
+            routeFilter = routeFilter ?? (context => true);
+
             var extractor = Propagations.B3String.Extractor<IHeaderDictionary>((carrier, key) => carrier[key]);
             app.Use(async (context, next) =>
             {
@@ -20,15 +23,18 @@ namespace zipkin4net.Middleware
                 
                 var trace = traceContext == null ? Trace.Create() : Trace.CreateFromId(traceContext);
                 Trace.Current = trace;
-                using (var serverTrace = new ServerTrace(serviceName, getRpc(context)))
+                if (routeFilter(request.Path))
                 {
-                    if (request.Host.HasValue)
+                    using (var serverTrace = new ServerTrace(serviceName, getRpc(context)))
                     {
-                        trace.Record(Annotations.Tag("http.host", request.Host.ToString()));
-                        trace.Record(Annotations.Tag("http.uri", UriHelper.GetDisplayUrl(request)));
+                        if (request.Host.HasValue)
+                        {
+                            trace.Record(Annotations.Tag("http.host", request.Host.ToString()));
+                            trace.Record(Annotations.Tag("http.uri", UriHelper.GetDisplayUrl(request)));
+                        }
+                        trace.Record(Annotations.Tag("http.path", request.Path));
+                        await serverTrace.TracedActionAsync(next());
                     }
-                    trace.Record(Annotations.Tag("http.path", request.Path));
-                    await serverTrace.TracedActionAsync(next());
                 }
             });
         }
